c5a5c5e0278ab52fcf6b0be7990a70d52e16b23a
Oluwaseun Odueso <seunoduez@gmail.com>
2025-12-28 00:08:11 +0000

    Initial Commit

---
+++ Guardian Angel/Guardian_AngelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guardian_AngelApp.swift
+//  Guardian Angel
+//
+//  Created by Oluwaseun Odueso on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Guardian_AngelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
